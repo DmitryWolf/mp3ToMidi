fb503787fd0f4c59eca062ff76408de7f30dd447
Dmitry Volkov <68951814+DmitryWolf@users.noreply.github.com>
2024-08-15 03:12:55 +0300

    update

--- consoleVersion/main.py
+++ consoleVersion/main.py
@@ -1,11 +1,11 @@
 import Converter
 
 def main():
+    converter = Converter.midiConverter(input_folder='input', output_folder='output', temp_folder='temp_dir', bpm=120, smooth=0.25)
     try:
-        converter = Converter.midiConverter(input_folder='input', output_folder='output', temp_folder='temp_dir', bpm=120, smooth=0.25)
+        converter.process_all_files()
     except Exception as e:
         print('Error: ', e)
-    converter.process_all_files()
 
 if __name__ == "__main__":
     main()
