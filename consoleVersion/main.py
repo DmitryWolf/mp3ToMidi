e4070e5ad88e251380147842543045663abf4088
Dmitry Volkov <68951814+DmitryWolf@users.noreply.github.com>
2024-08-15 03:09:50 +0300

    update

--- consoleVersion/main.py
+++ consoleVersion/main.py
@@ -1,7 +1,10 @@
 import Converter
 
 def main():
-    converter = Converter(input_folder='input', output_folder='output', temp_folder='temp_dir', bpm=120, smooth=0.25)
+    try:
+        converter = Converter.midiConverter(input_folder='input', output_folder='output', temp_folder='temp_dir', bpm=120, smooth=0.25)
+    except Exception as e:
+        print('Error: ', e)
     converter.process_all_files()
 
 if __name__ == "__main__":
